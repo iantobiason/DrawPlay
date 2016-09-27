b7cf9e102266f83e3cfed651deffff7f3d33f01b
Christian McErlean <cmcerlea@mines.edu>
2016-09-27 12:09:06 -0600

    Make cat a polite member of society

--- src/Cat.java
+++ src/Cat.java
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Wanna buy some weed, you GOOKS", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Hello, how are you?", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
